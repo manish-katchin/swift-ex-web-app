2533c4eb61aa96c305a1e060a617ab46ca6bc91f
manish-katchin <manish@katchintech.com>
2025-11-08 17:57:34 +0530

    updated the port

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/subscribe', subscriberRoutes);
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
